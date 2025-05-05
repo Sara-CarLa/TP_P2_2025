c9f8d6e17981c4139703ec7ee603f116180a0124
Alumnos <cardenaslaguialejandra@gmail.com>
2025-05-05 11:01:00 -0600

    Primera parte de la practica

---
+++ SistemaGimnasio/SistemaGimnasio/Modelos/Rutina.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace SistemaGimnasio.Modelos
+{
+    public class Rutina
+    {
+        //Propiedades: se requiere al instanciar el objeto
+        public string Nombre { get; set; }
+        public int Duracion { get; set; }
+        //Variable de clase: se construte en el flujo del programa
+        private List<Ejercicio> ejercicios;
+
+        //Constructor
+        public Rutina(string nombre, int duracion)
+        {
+            Nombre = nombre;
+            Duracion = duracion;
+        }
+
+        //Métodos
+        public void AgregarEjercicio(Ejercicio ejercicio)
+        {
+            ejercicios.Add(ejercicio);
+        }
+
+        public List<Ejercicio> ObtenerEjercicios()
+        {
+            return ejercicios;
+        }
+
+        public void BorrarEjercicio(Ejercicio ejercicio)
+        {
+            ejercicios.Remove(ejercicio);
+        }
+         
+            
+            
+            
+
+
+
+    }
+}
